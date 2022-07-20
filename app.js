1350c30fc54453337ff72c70c622a90433ea9475
rebeca <reb19loza@gmail.com>
2022-07-20 22:20:49 +0200

    interaciones terminadas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const hbs = require('hbs');
+
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -15,11 +16,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
-
+const hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(data => {
+    let beersFromApi = data
+    res.render("beers.hbs", {beersFromApi})
+   // console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+  
+})
+
+
+app.get("/random-beer", (req, res) =>{
+  punkAPI.getRandom()
+  .then( data => {
+    let responseFromAPI = data
+    // your magic happens here
+    res.render("random-beer.hbs", {responseFromAPI})
+  })
+  .catch(error => console.log(error));
+  
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
